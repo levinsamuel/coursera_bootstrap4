1113eee4735ed1ee1b947a73e4a10ef9319b5b3f
Samuel Levin <levinsamuel000@gmail.com>
2018-10-23 20:01:22 +0200

    script refactoring, add onchange to scss

---
+++ webapp/scripts/js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  $('#LoginModal').on('shown.bs.modal', function(){
+
+  });
+  $('#LoginButton').click(function(){
+    $('#LoginModal').modal('show');
+  });
+  $('#ReserveButton').click(function(){
+    $('#ReservationsModal').modal('show');
+  });
+
+  $('#mycarousel').carousel({interval:2000});
+  $('#carousel-button').click(function(){
+    console.log('carousel button');
+    if ($('#carousel-button').children('span').hasClass('fa-pause')) {
+      $('#mycarousel').carousel('pause');
+      $('#carousel-button').children('span').removeClass('fa-pause');
+      $('#carousel-button').children('span').addClass('fa-play');
+    } else if ($('#carousel-button').children('span').hasClass('fa-play')) {
+      $('#mycarousel').carousel('cycle');
+      $('#carousel-button').children('span').removeClass('fa-play');
+      $('#carousel-button').children('span').addClass('fa-pause');
+    }
+  });
+});
